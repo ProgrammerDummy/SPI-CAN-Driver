4920340b7b4cd1a56513844f84100328956e959a
Matthew Jeong <mangjee.24@gmail.com>
2026-02-20 18:34:30 -0500

    Add blinker test file

---
+++ blinker.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/timer.h"
+
+int main() {
+
+   
+}
+
+
+/*
+
+    pico_stdlib    
+    hardware_gpio    
+    hardware_timer  
+*/
